c058ae258901afc093ea6744a5fcab83115e7903
CRYPTRIXLEGEND <tanishbaliyan07@gmail.com>
2025-12-26 10:02:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+const canvas = document.getElementById("snow");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let snowflakes = [];
+
+for (let i = 0; i < 150; i++) {
+  snowflakes.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    r: Math.random() * 4 + 1,
+    d: Math.random() + 1
+  });
+}
+
+function drawSnow() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "white";
+  ctx.beginPath();
+
+  for (let i = 0; i < snowflakes.length; i++) {
+    let f = snowflakes[i];
+    ctx.moveTo(f.x, f.y);
+    ctx.arc(f.x, f.y, f.r, 0, Math.PI * 2);
+  }
+  ctx.fill();
+  moveSnow();
+}
+
+function moveSnow() {
+  for (let i = 0; i < snowflakes.length; i++) {
+    let f = snowflakes[i];
+    f.y += f.d;
+    if (f.y > canvas.height) {
+      f.y = 0;
+      f.x = Math.random() * canvas.width;
+    }
+  }
+}
+
+setInterval(drawSnow, 30);
